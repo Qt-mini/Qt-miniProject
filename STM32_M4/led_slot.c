7a8b7ee62a331820d3f762fc04b0f41150e16f2f
rlagudwls5518 <rlagudwls5518@naver.com>
2026-08-17 17:00:41 +0900

    feat: led_slot.c 함수 수정 - Slot_SetOccupied, Slot_SetEmpty 구현 - getter구현 - 올바르지 못한 층 입력시 에러 추가

--- STM32_M4/led_slot.c
+++ STM32_M4/led_slot.c
@@ -1,37 +1,43 @@
 #include "device_driver.h"
 
-#define first_floor   0b00000001
-#define second_floor  0b00000010
-#define third_floor   0b00000100
-#define fourth_floor  0b00001000
-#define fifth_floor   0b00010000
-#define sixth_floor   0b00100000
-#define seventh_floor 0b01000000
-#define eighth_floor  0b10000000
-#define empty_floor   0b00000000
+#define EMPTY_FLOOR   0b00000000
 #define FULL_FLOOR    0b11111111
 #define SLOT_ERROR    -1
 
-static const unsigned int floor_mask[8] = {first_floor, second_floor, third_floor, 
-    fourth_floor, fifth_floor, sixth_floor, seventh_floor, eighth_floor};
+static unsigned int g_slot_occupancy_mask = EMPTY_FLOOR;
+int error_input(int floor);
 
-static unsigned int g_slot_occupancy_mask = 0b00000000;
 
-unsigned int Slot_FindLowestEmpty(){
+int Slot_FindLowestEmpty(){
     if(g_slot_occupancy_mask == FULL_FLOOR){
         return SLOT_ERROR;
     }
     for (int i = 0; i < 8; i++) {
-        if ((g_slot_occupancy_mask & floor_mask[i]) == 0) {
-            return i+1; 
+        if (Macro_Check_Bit_Clear(g_slot_occupancy_mask, i)) {
+            return i; 
         }
     }
-    return SLOT_ERROR;
 }
-// void Slot_SetOccupied(unsigned int floor){
+void Slot_SetOccupied(unsigned int floor){
+    if (error_input(floor) || Macro_Check_Bit_Set(g_slot_occupancy_mask, floor)) {
+        return;
+    }
+    Macro_Set_Bit(g_slot_occupancy_mask, floor);
+}
+void Slot_SetEmpty(unsigned int floor){
+    if (error_input(floor) || Macro_Check_Bit_Clear(g_slot_occupancy_mask, floor)) {
+        return;
+    }
+    Macro_Clear_Bit(g_slot_occupancy_mask, floor);
+}
 
-// }
-// void Slot_SetEmpty(unsigned int floor){
+unsigned int get_slot(){
+    return g_slot_occupancy_mask;
+}
 
-// }
+
+int error_input(int floor){
+    if(floor >= 8 || floor < 0) return 1;
+    return 0;
+}
 
