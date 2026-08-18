d571ae69ed8e5d36261eb412cf72128dc1386a32
rlagudwls5518 <rlagudwls5518@naver.com>
2026-08-18 09:18:02 +0900

    fix: led.c 수정 - led_init함수  led초기화 내부 메크로 수정

--- STM32_M4/led.c
+++ STM32_M4/led.c
@@ -4,15 +4,7 @@ void LED_Init(void)
 {
 	Macro_Set_Bit(RCC->AHB1ENR, 0); 
 
-	Macro_Write_Block(GPIOC->MODER, 0x3, 0x1, 10);
-
-	Macro_Clear_Bit(GPIOC->OTYPER, 0);
-	Macro_Clear_Bit(GPIOC->OTYPER, 1);
-	Macro_Clear_Bit(GPIOC->OTYPER, 2);
-	Macro_Clear_Bit(GPIOC->OTYPER, 3);
-	Macro_Clear_Bit(GPIOC->OTYPER, 4);
-	Macro_Clear_Bit(GPIOC->OTYPER, 5);
-	Macro_Clear_Bit(GPIOC->OTYPER, 6);
+	Macro_Write_Block(GPIOC->MODER, 0xffff, 0x5555, 0);
 
 
 	Macro_Clear_Bit(GPIOC->ODR, 0);
